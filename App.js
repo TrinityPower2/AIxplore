0d6d4f85de99d93f50486f23b593c7c90390f27d
Kunthearo <nothyroo@gmail.com>
2024-05-31 17:21:05 +0200

    Test_Connection_DB_Back_Front

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import axios from 'axios';
 
 export default function App() {
+
+  const fetchData = async () => {
+    try {
+      let response = await fetch('http://16.16.96.254/');
+      let json = await response.json();
+      Alert.alert("Response from server", json.message);
+    } catch (error) {
+      console.error(error);
+      Alert.alert("Error", "Failed to connect to backend");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Appuyer ici" onPress={fetchData}></Button>
       <StatusBar style="auto" />
     </View>
   );
