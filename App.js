cc1f8baacf20086ef59f882f75afbfee343b9e81
Kunthearo <nothyroo@gmail.com>
2024-06-06 13:48:42 +0200

    Delete Landing + Create Register

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Landing from '../AIxplore/js/Landing';
 import Login from '../AIxplore/js/Login';
 
 const Stack = createNativeStackNavigator();
@@ -9,8 +8,7 @@ const Stack = createNativeStackNavigator();
 function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Landing" screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="Landing" component={Landing} />
+            <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="Login" component={Login} />
             </Stack.Navigator>
         </NavigationContainer>
--- js/Login.js
+++ js/Login.js
@@ -1,9 +1,28 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { View, Animated, StyleSheet, Text, TextInput, Button, Image } from 'react-native';
 
-const Login = ({ navigation }) => {
+const SplashPage = () => {
+    const logoOpacity = useRef(new Animated.Value(0)).current;
+    const logoMoveY = useRef(new Animated.Value(0)).current;
+    const [showLogin, setShowLogin] = useState(false);
+
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+
+    useEffect(() => {
+        Animated.sequence([
+            Animated.timing(logoOpacity, {
+                toValue: 1,
+                duration: 3000,
+                useNativeDriver: true
+            }),
+            Animated.timing(logoMoveY, {
+                toValue: -225,
+                duration: 1000,
+                useNativeDriver: true
+            })
+        ]).start(() => setTimeout(() => setShowLogin(true), 500));
+    }, [logoOpacity, logoMoveY]);
 
     const handleLogin = () => {
         console.log(username, password);
@@ -11,39 +30,85 @@ const Login = ({ navigation }) => {
 
     return (
         <View style={styles.container}>
-            <Text>Login Page</Text>
-            <TextInput
-                style={styles.input}
-                onChangeText={setUsername}
-                value={username}
-                placeholder="Username"
-            />
-            <TextInput
-                style={styles.input}
-                onChangeText={setPassword}
-                value={password}
-                placeholder="Password"
-                secureTextEntry
-            />
-            <Button title="Login" onPress={handleLogin} />
+            <Animated.Image source={require('../assets/real_logo.png')}
+                style={[styles.logo, { opacity: logoOpacity, transform: [{ translateY: logoMoveY }] }]} />
+            {showLogin && (
+                <View style={styles.loginContainer}>
+                    <CustomInput
+                        imageSource={require('../assets/icon_username.png')}
+                        onChangeText={setUsername}
+                        value={username}
+                        placeholder="Email"
+                    />
+                    <CustomInput
+                        imageSource={require('../assets/icon_password.png')}
+                        onChangeText={setPassword}
+                        value={password}
+                        placeholder="Password"
+                        secureTextEntry={true}
+                    />
+                    <Button title="Login" onPress={handleLogin} />
+                </View>
+            )}
         </View>
     );
 };
+
+const CustomInput = ({ imageSource, onChangeText, value, placeholder, secureTextEntry }) => (
+    <View style={styles.inputContainer}>
+        <Image source={imageSource} style={styles.icon} />
+        <TextInput
+            style={styles.input}
+            onChangeText={onChangeText}
+            value={value}
+            placeholder={placeholder}
+            secureTextEntry={secureTextEntry}
+            underlineColorAndroid="transparent"
+        />
+    </View>
+);
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#D5D4E5'
+        backgroundColor: '#384454',
     },
-    input: {
-        height: 40,
+    logo: {
+        width: 225,
+        height: 225,
+        resizeMode: 'contain'
+    },
+    loginContainer: {
+        position: 'absolute',
+        top: '50%',
+        left: '10%',
+        width: '80%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#384454'
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
         margin: 12,
         borderWidth: 1,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 10,
         padding: 10,
-        width: '80%'
-    }
+    },
+    input: {
+        flex: 1,
+        height: 40,
+        textAlign: 'left',
+        color: '#000'
+    },
+    icon: {
+        width: 20,
+        height: 20,
+        marginRight: 10,
+    },
 });
 
-export default Login;
+export default SplashPage;
